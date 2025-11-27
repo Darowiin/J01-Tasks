dbc35ec322af1e86a95e82d17adae68433300506
Darowiin <vlad.tsunenko.2004@mail.ru>
2025-11-27 12:04:45 +0400

    encapsulate PriorityNode as private static nested class

--- src/main/java/stack/PriorityStack.java
+++ src/main/java/stack/PriorityStack.java
@@ -15,7 +15,7 @@ public class PriorityStack<T> {
     /**
      * Ссылка на первый (верхний) элемент стека.
      */
-    public PriorityNode<T> head;
+    private PriorityNode<T> head;
 
     /**
      * Возвращает количество элементов в стеке.
@@ -91,5 +91,40 @@ public class PriorityStack<T> {
         }
 
         return this.head.value;
+    }
+
+    /**
+     * Узел односвязного списка, использующийся в {@link PriorityStack}.
+     *
+     * @param <T> тип хранимого значения
+     */
+    private static class PriorityNode<T> {
+
+        /**
+         * Значение, хранящееся в узле.
+         */
+        public T value;
+
+        /**
+         * Приоритет элемента (меньшее число = выше приоритет).
+         */
+        public int priority;
+
+        /**
+         * Ссылка на следующий узел в списке.
+         */
+        public PriorityNode<T> next;
+
+        /**
+         * Создаёт новый узел с указанным значением и приоритетом.
+         *
+         * @param value    значение в узле
+         * @param priority приоритет элемента
+         */
+        public PriorityNode(T value, int priority) {
+            this.value = value;
+            this.priority = priority;
+            this.next = null;
+        }
     }
 }
